29a02dac8f2d112c44104dda74f26d125afdc459
James Fraser <wulfgar.pro@gmail.com>
2025-07-19 08:28:32 +1000

    Add solution template

---
+++ soln.py
@@ -0,0 +1,15 @@
+from pwn import *
+
+context.log_level = "debug"
+context.terminal = ["wezterm", "start", "--"]
+
+bin = "XXX"
+p = process(bin)
+gdb.attach(p, api=True)
+e = ELF(bin)
+rop = ROP(e)
+
+payload = b""
+
+p.sendafter(b"> ", payload)
+p.recvall()
