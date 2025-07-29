c90529a152437f0cf7b26ce9afe36c72ad71ea32
James Fraser <wulfgar.pro@gmail.com>
2025-07-29 13:05:47 +1000

    Add explanation to "fluff" solution
    Removed old `soln2.py` that was accidentally committed.

--- x86_64/6_fluff/soln.py
+++ x86_64/6_fluff/soln.py
@@ -10,6 +10,79 @@ e = ELF(bin)
 rop = ROP(e)
 
 """
+This challenge required the use of "weird" gadgets to perform an arbitrary
+write, namely `bextr`->`xlatb`->`stosb`, instead of a clean `mov [rX], rY`.
+
+These gadgets are "byte wise" write gadgets, used in order:
+
+    - `bextr rbx, rcx, rdx` to set RBX for the following `xlatb` (in this form,
+      `bextr` sets RBX to the bit-field from RCX using RDX as index+len).
+
+    - `xlatb` to set AL (1 byte) from RBX (`xlatb` sets AL to [RBX + AL]).
+
+    - `stosb byte ptr [rdi], al` to set the memory address at RDI from AL,
+      walking RDI forward byte-by-byte (`stosb` stores a byte from AL at
+      address RDI).
+
+Where these instructions are named as follows:
+
+    - BEXTR: Bit Field Extract
+    - XLATB: Table Look-up Translation
+    - STOSB: Store String Byte
+
+Therefore, by setting RDI to the start of the `.data` section, we can use the
+above sequence to write byte-by-byte the string "flag.txt" to RDI.
+
+Plan:
+-----
+
+1. Set RDI to the `.data` start address (destination for the write).
+
+    - `pop rdi; ret`
+
+2. Clear AL (needed so the first `xlatb` indexes from RBX+0)
+
+    - `mov eax, 0; pop rbp; ret` -> feed junk for RBP,
+      e.g. `0xDEADBEEFDEADBEEF`.
+
+3. For each byte `ch` in "flag.txt":
+
+    a. Find an address to a `ch` in the binaries mapped memory:
+       `ch_addr = next(e.search(p8(b)))`.
+
+    b. Load that address into RBX using the `bextr` gadget:
+        `pop rdx; pop rcx; add rcx, 0x3ef2; bextr rbx, rcx, rdx; ret`.
+
+        - We set RDX to `0xFF00`, so that the index is `0` and len is `0xFF`.
+        - We set RXC to `ch_addr - 0x3ef2 - prev_al` so that after the gadgets
+          `add rcx, 0x3ef2`, RBX points to `(ch_addr - prev_al)`.
+
+    c. Use the `xlatb` gadget to set AL from RBX.
+
+4. Set RDI back to the `.data` start (because `stosb` walked it forward 8 bytes
+   ("flag.txt") from the initial start address of `.data`) and call
+   `print_file()`.
+
+(Optionally drop a `ret` for stack alignment if necessary...)
+
+Weird Gadgets:
+--------------
+
+pop rdx; pop rcx; add rcx, 03ef2; bextr rbx, rcx, rds; ret
+
+    - pop rdx             : control bits for BEXTR; len is bits 15:8, index is bits 7:0
+    - pop rcx             : value BEXTR extracts bits from
+    - add rcx, 0x3ef2     : constant offset we have to account for later
+    - bextr rbx, rcx, rdx : set RBX to bits in RCX, starting from :index in RDX
+    - ret
+
+xlatb; ret
+
+    - set AL (1-byte) from [RBX + AL]; treats AL as index into RBX
+
+stosb byte ptr [rdi], al; ret
+
+    - write AL to [RDI], then increment RDI (walks forward one byte each use)
 """
 
 # NOTE: `pwntools` can only find simple gadgets, see:
@@ -73,8 +146,6 @@ for ch in flag_str:
 # Write `data_section_addr` to RDI again (since current RDI was auto incremented)
 payload += p64(pop_rdi_ret_addr)
 payload += p64(data_section_addr)
-# debug(f"pop_rdi_ret_addr = {hex(pop_rdi_ret_addr)}")
-# debug(f"data_section_addr = {hex(data_section_addr)}")
 # payload += p64(ret_addr)  # Align stack
 # Call `print_file()`
 payload += p64(print_file_plt)
